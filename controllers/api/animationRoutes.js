df454b1a89dd20c1000e98326c2b3bfba8e644f4
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-11-03 11:22:03 -0700

    added animation get routes

--- controllers/api/animationRoutes.js
+++ controllers/api/animationRoutes.js
@@ -1,9 +1,41 @@
-var router = require('express').Router();
-var {Animation, User} = reqiure('../../models');
+const router = require('express').Router();
+const uuid = require()
+const fs = require('fs');
+const {Animation, User} = require('../../models');
 
-// GET animation
+// GET all animations
+router.get('/', async (req, res) => {
+    try {
+        const animations = await Animation.findAll({
+            include: [{model: User}]
+        });
+        const animationsJSON = animations.map((animation) => {
+            animation.get({plain: true});
+        })
+        res.status(200).json(animationsJSON);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
-// POST animation
+// GET a given animation
+router.get('/:id', async (req, res) => {
+    try {
+        const animation = await Animation.findByPk(req.params.id, {
+            include: [{model: User}]
+        });
+        if(!animation) {
+            res.status(404).json({response: `Animation of id ${req.params.id} not found`});
+        }
+        const animationJSON = animation.get({ plain:true });
+        res.status(200).json(animationJSON);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// POST animation (given stringified JSON from view in body)
+
 
 // PUT (update) animation
 

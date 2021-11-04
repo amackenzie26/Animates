9dcfec3efb3219c2ce5bbe35d46803c224675852
Matt Weichel <maweiche@gmail.com>
2021-11-04 15:27:03 -0700

    resolved issue with login/signup by linking  script and fixing button ids

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require ('express').Router();
 const { User, Post, Comment } = require('../../models');
+// const { rawAttributes } = require('../../models/User');
 
 //GET /api/users for 'all' users
 router.get('/', (req, res) => {
@@ -24,7 +25,7 @@ router.get('/:id', (req, res) => {
         include: [
             {
                 model: Post,
-                attributes: ['id', 'name', 'description', 'date_created', 'user_id']
+                attributes: ['id', 'username', 'description', 'date_created', 'user_id']
             }
         ]
         .then(dbUserData => {
@@ -44,58 +45,54 @@ router.get('/:id', (req, res) => {
 
 //CREATE new User
 router.post('/', async (req, res) => {
-    try {
-        const salts = await brcrypt.genSalt(10)
-        const newPw = await brcrypt.hash(req.body.password, salt)
-        const userData = await User.findByIdAndUpdate(req.params.user_id, {
-            username: req.body.username,
-            password: newPw
-        })
-
+    try{
+    console.log("this is our signup info")
+    console.log(req.body)
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    //store user data during session
+    .then(dbUserData => {
         req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
 
-            res.status(200).json(userData);
-        });
-    } catch (err) {
+            res.status(200).json(dbUserData);
+        })
+    })}
+    catch(err){
+        console.error(err)
         res.status(500).json(err);
     }
 });
-
-
 //LOG-IN for users and PW Validation
 router.post('/login', async (req, res) => {
-    try {
-        const userData = await User.findOne({ where: {
-            email: req.body.email 
-        }});
-
-        if (!userData) {
-            res
-            .status(400)
-            .json({ message: 'Incorrect email, please try again'});
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(dbUserData => {
+        if(!dbUserData) {
+            res.status(400).json({ message: 'No user with matching username.'})
             return;
         }
+        const validPassword = dbUserData.checkPassword(req.body.password);
 
-        const validPassword = await userData.checkPassword(req.body.password);
-
-        if (!validPassword) {
-            res
-            .status(400)
-            .json ({ message: 'Incorrect email or password, please try again'});
+        if(!validPassword) {
+            res.status(400).json({ message: 'Inccorect password.' });
             return;
         }
-
         req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
 
-            res.json({ user: userData, message: 'You are logged in!'});
+            res.json({ user: dbUserData, message: 'Login successful. '});
         });
-    } catch (err) {
-        res.status(400).json(err);
-    }
+    });
 });
 
 //LOG-OUT for User
--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,11 +1,11 @@
 async function signupFormHandler(event) {
     event.preventDefault();
-
+    console.log("signing up user")
     //get data from form
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-
+    console.log(username, email, password)
     if(username && email && password) {
         const response = await fetch('/api/users', {
             method: 'post',
@@ -19,11 +19,11 @@ async function signupFormHandler(event) {
         //check response status
         if (response.ok) {
             console.log('success');
-            document.location.replace('/dashboard');
+            document.location.replace('/');
         } else {
             alert(response.statusText);
         }
     }
 }
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('#signup').addEventListener('click', signupFormHandler);

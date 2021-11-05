a2a3a10eea450e41679ab22e8902df45ac98ed34
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-11-04 21:26:46 -0700

    added get animations page

--- controllers/index.js
+++ controllers/index.js
@@ -3,15 +3,13 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboardRoutes.js');
+const animationRoutes = require('./animations');
 
 
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/', homeRoutes);
-
-router.use('/create/animation', async (req, res) => {
-    res.render('create-animation');
-});
+router.use('/animations', animationRoutes);
 
 router.use((req, res) => {
     res.status(404).end();

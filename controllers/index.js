b8e272af99f9e3e761586d2cd47a4c668d00633d
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-11-04 18:08:18 -0700

    added view layer for create-animation

--- controllers/index.js
+++ controllers/index.js
@@ -7,6 +7,10 @@ const homeRoutes = require('./home-routes.js');
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
+router.use('/create/animation', async (req, res) => {
+    res.render('create-animation');
+});
+
 router.use((req, res) => {
     res.status(404).end();
 });

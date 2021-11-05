6aa5581fa7ec743015cbf6fa23131cb73cf4500d
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-11-04 21:36:28 -0700

    fixed logout issue

---
+++ controllers/animations.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { response } = require('express');
+const { Animation } = require('../models');
+
+router.get('/create', async (req, res) => {
+    try {
+    res.render('create-animation');
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const animation = await Animation.findByPk(req.params.id);
+        const animationJSON = await animation.get({ plain: true });
+        res.render('animation', {
+            loggedIn: req.session.loggedIn,
+            animation: animationJSON
+        });
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+
+
+module.exports = router;
--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,10 +1,12 @@
 async function logout() {
+    console.log('hello world');
     const response = await fetch('/api/users/logout', {
         method: 'post',
         headers: { 'Content-Type': 'application/json' }
     });
 
     if (response.ok) {
+        console.log('response ok');
         document.location.replace('/');
     } else {
         alert(response.statusText);
